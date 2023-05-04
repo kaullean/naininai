d6a92a5609b36164f732375656508933cf9edb3e
Leandro Kaul <leandrokaul@hotmail.com>
2023-05-04 16:32:04 -0300

    proyecto

---
+++ desafio3.js
@@ -0,0 +1,13 @@
+function getCodigo(){
+    if(document.getElementById('imagenCodigo')){
+        document.getElementById("imagenCodigo").remove();
+    }
+    var codigo = document.getElementById('codigoDeBarras').value;
+    var url = 'https://barcode.tec-it.com/barcode.ashx?data='+codigo+'&code=EAN13&translate-esc=on';
+    var image = new Image();
+    image.src = url;
+    image.id= "imagenCodigo";
+    document.getElementById('container').appendChild(image);
+
+}
+
