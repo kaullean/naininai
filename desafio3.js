8017c99e339412d6e8daecb227a539a5c64388f8
Leandro Kaul <leandrokaul@hotmail.com>
2023-05-05 16:09:17 -0300

    version celu

--- desafio3.js
+++ desafio3.js
@@ -1,3 +1,19 @@
+function onKeyDownHandler(event) {
+
+    var codigo = event.which || event.keyCode;
+
+    console.log("Presionada: " + codigo);
+     
+    if(codigo === 13){
+      console.log("Tecla ENTER");
+    }
+
+    if(codigo >= 65 && codigo <= 90){
+      console.log(String.fromCharCode(codigo));
+    }
+
+     
+}
 function getCodigo(){
     if(document.getElementById('imagenCodigo')){
         document.getElementById("imagenCodigo").remove();
